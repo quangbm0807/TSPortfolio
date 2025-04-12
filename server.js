32e65207c61f6dfe1646ebc83a383822adccdf5c
Quang <135381994+quangbm0807@users.noreply.github.com>
2025-04-12 21:20:34 +0700

    ok

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fetch from 'node-fetch';
 import path from 'path';
+import nodemailer from 'nodemailer';
 
-// Thay thế __dirname trong ES modules
+// Replace __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
